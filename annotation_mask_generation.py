7683fec276b296ed4487950867fd9638dc78eb57
Huang <yuhao.huang@phsabc.ehcnet.ca>
2018-11-22 10:44:47 -0800

    initialization

---
+++ annotation_mask_generation.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  2 14:50:56 2018
+
+@author: Yuhao Huang
+"""
+
+
+import pydicom
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from PIL import Image
+import pylab
+
+#
+
